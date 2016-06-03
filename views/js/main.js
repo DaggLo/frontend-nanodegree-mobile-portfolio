51db1e888d0facf3e8c7ada24872872ffdbfd920
Evgeny Degtev <e.a.degtev@gmail.com>
2016-06-03 16:03:53 +0300

    refactor: Fix FSL in changePizzaSizes().

--- views/js/main.js
+++ views/js/main.js
@@ -450,10 +450,17 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    var randomPizzaContainerNodeList = document.querySelectorAll(".randomPizzaContainer");
+    var newwidthArr = [];
+
+    for (var i = 0; i < randomPizzaContainerNodeList.length; i++) {
+      var dx = determineDx(randomPizzaContainerNodeList[i], size);
+      var newwidth = (randomPizzaContainerNodeList[i].offsetWidth + dx) + 'px';
+      newwidthArr.push(newwidth);
+    }
+
+    for (i = 0; i < randomPizzaContainerNodeList.length; i++) {
+      randomPizzaContainerNodeList[i].style.width = newwidthArr[i];
     }
   }
 
