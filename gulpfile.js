f14dac8d50127dc5b51fadf33e5f01533501536f
Evgeny Degtev <e.a.degtev@gmail.com>
2016-07-06 17:28:33 +0300

    chore: Refactor gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@
   // - gulp-imagemin (Images minification)
   // - gulp-csso (CSS minification)
   // - gulp-uglify (JS minification)
+  // - pump (Debugging)
   // - del (cleaning build directory)
 //=========================================================//
 
@@ -13,6 +14,7 @@ var gulp = require('gulp'),
   cssMin = require('gulp-csso'),
   imageMin = require('gulp-imagemin'),
   jsMin = require('gulp-uglify'),
+  pump = require('pump'),
   del = require('del');
 
 gulp.task('imagemin', function() {
@@ -22,25 +24,45 @@ gulp.task('imagemin', function() {
 });
 
 gulp.task('cssmin', function() {
-  gulp.src('app/css/**/*.css')
+  gulp.src('app/**/*.css')
     .pipe(cssMin({sourceMap: true}))
     .pipe(gulp.dest('develope'));
 });
 
 gulp.task('jsmin', function() {
   pump([
-    gulp.src('app/js/**/*.js'),
+    gulp.src('app/**/*.js'),
     jsMin(),
     gulp.dest('develope')
   ]);
 });
 
-gulp.task('optimize', ['del'], function() {
-  gulp.watch('app/img/**/*.+(jpg|png|svg|jpeg)', ['imagemin']);
-  gulp.watch('app/css/**/*.css', ['cssmin']);
-  gulp.watch('app/css/**/*.js', ['jsmin']);
-  gulp.watch('app/**/*.html', function() {
-    gulp.src('app/**/*.html')
-      .pipe(gulp.dest('develope'));
-  });
+gulp.task('html', function() {
+  gulp.src('app/**/*.html')
+    .pipe(gulp.dest('develope'));
+})
+
+gulp.task('clean', function(cb) {
+  del(['develope/**']);
+  cb();
+});
+
+gulp.task('dev', ['clean'], function(cb) {
+  gulp.watch('app/**/*.+(jpg|png|svg|jpeg)', ['imagemin']);
+  gulp.watch('app/**/*.css', ['cssmin']);
+  gulp.watch('app/**/*.js', ['jsmin']);
+  gulp.watch('app/**/*.html', ['html']);
+  cb();
+  // заменить вызовы задач на кол-бэки.
+});
+
+gulp.task('opti-forced', ['imagemin', 'cssmin', 'jsmin', 'html']);
+
+gulp.task('build', ['opti-forced'], function() {
+  del(['build/**']);
+  gulp.src(['develope/**', '!develope/**/*.css'])
+    .pipe(gulp.dest('build'));
+  gulp.src('app/**/*.css')
+    .pipe(cssMin())
+    .pipe(gulp.dest('build'));
 });
