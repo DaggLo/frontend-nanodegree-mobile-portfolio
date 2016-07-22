d6c643f24c5e355a497ed97abcc5519243cfcf44
Evgeny Degtev <e.a.degtev@gmail.com>
2016-07-22 17:12:57 +0300

    refactor: Change number of moving pizzas cols to 10.

--- app/views/js/main.js
+++ app/views/js/main.js
@@ -536,7 +536,7 @@ window.addEventListener('scroll', updatePositions);
 
 // Generates the sliding pizzas when the page loads.
 document.addEventListener('DOMContentLoaded', function() {
-  var cols = 8;
+  var cols = 10;
   var s = 256;
   for (var i = 0; i < 80; i++) {
     var elem = document.createElement('img');
